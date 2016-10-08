5f7d9861c41c4c0b5fb7f6eae5020252296e5fde
Brenton Sutherland <brentonsutherland311@gmail.com>
2016-10-08 16:18:30 -0400

    Added finish a rental; update location.

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import *
 from peewee import *
+from datetime import datetime, timedelta
+import string
+import random
 
 app = Flask(__name__)
 
@@ -33,6 +36,90 @@ class ActiveRentals(Model):
     class Meta:
         db_table = 'active_rentals'
         database = db_rentals
+
+class UserInfo(Model):
+    id = PrimaryKeyField()
+    username = TextField()
+    password = TextField()
+    fullname = TextField()
+
+    class Meta:
+        db_table = 'user_info'
+        database = db_rentals
+
+@app.route('/stoprental/', methods=['POST'])
+def updateActiveRental():
+    rental_id = request.form['rental_id']
+    rental = ActiveRentals.get(ActiveRentals.id == rental_id)
+    rental.delete_instance()
+
+    return 'Ok'
+
+@app.route('/rentals/')
+def rentalsPage():
+    """ Display posted rentals """
+    str_output = '<b>Posted Rentals</b><br>'
+    for rental in PostedRentals.select():
+        str_output += '{}: {}, Available from {} to {}, {}, {}, ${}, {}'.format(
+                        rental.id,
+                        rental.car_info,
+                        rental.dt_begin, 
+                        rental.dt_end,
+                        rental.is_vacant,
+                        rental.location,
+                        rental.price,
+                        rental.user_key)
+        str_output += '<br>'
+
+    str_output += '<b>Active Rentals:</b><br>'
+    for rental in ActiveRentals.select():
+        str_output += '{}: {}, {}, {}, Paid: ${}, {}, {}km/h, {}'.format(
+                    rental.id,
+                    rental.posting,
+                    rental.dist_travelled, 
+                    rental.is_completed,
+                    rental.is_paid,
+                    rental.latest_loc,
+                    rental.max_speed,
+                    rental.nfc_code)
+        str_output += '<br>'
+
+    return str_output
+
+def getRandomString(size=8, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+@app.route('/book/', methods=['POST'])
+def bookRental():
+    try:    
+        rental_id = request.form['rental_id']
+        rental = PostedRentals.get(PostedRentals.id == rental_id)
+        
+        # Car is already rented
+        if not rental.is_vacant:
+            return 'Not Vacant'
+        
+        current_time = datetime.utcnow()
+        print(current_time.strftime(__datetime_frt__))
+
+        rental.is_vacant = False
+
+        active_rental = ActiveRentals(posting=rental.id, 
+                                      dist_travelled=0,
+                                      is_completed=False,
+                                      is_paid=False,
+                                      latest_loc=rental.location,
+                                      max_speed=0,
+                                      nfc_code=getRandomString())
+
+        rental.save()
+        active_rental.save()
+
+        # Give the client their NFC code
+        return active_rental.nfc_code
+    except Exception as e:
+        return 'Error' + str(e)
 
 @app.route('/deleterental/', methods=['POST'])
 def rmRental():
@@ -68,43 +155,42 @@ def addRental():
         print(e)
         return 'Error occured'
 
-@app.route('/book/', methods=['POST'])
-def bookRental():
+@app.route('/login/', methods=['POST'])
+def login():
+    """Mock login procedure"""
+    try:
 
+        username = request.form['username']
+        password = request.form['password']
 
-    return 'Hi'
-
-@app.route('/rentals/')
-def rentalsPage():
-    """ Display rentals """
-    str_output = ''
-    for rental in PostedRentals.select():
-        str_output += '{}: {}, Available from {} to {}, {}, {}, ${}, {}'.format(
-                        rental.id,
-                        rental.car_info,
-                        rental.dt_begin, 
-                        rental.dt_end,
-                        rental.is_vacant,
-                        rental.location,
-                        rental.price,
-                        rental.user_key)
-        str_output += '<br>'
-
-    return str_output
+        user = UserInfo.get(UserInfo.username == username and UserInfo.password == password)
+        if(user is None):
+            return 'Failure!'
+        
+        # return 'Welcome {}'.format(user.fullname)
+        return str(user.id)
+    except:
+        return 'Error'
 
 @app.route('/updatelocation/', methods=['POST'])
 def updateLocation():
     if not 'location' in request.form:
         return 'Invalid location update!'
 
+    rental_id = request.form['rental_id']
+    rental = ActiveRentals.get(ActiveRentals.posting == rental_id)
+    
     loc = request.form['location']
     loc_data = loc.split(',')
     lat = float(loc_data[0])
     lon = float(loc_data[1])
+    alt = float(loc_data[2])
+    heading = float(loc_data[3])
 
+    rental.latest_loc = '{}, {}, {}, {}'.format(lat, lon, alt, heading)
+    rental.save()
 
-    return str(loc_data) + '\n'
-
+    return 'Ok'
 
 """Launch the app and make it externally visible"""
 if __name__ == '__main__':
